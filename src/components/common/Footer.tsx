2dec4fe8ab712910e6a9d15596fa0702d2537b14
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2024-10-17 16:39:51 -0500

    refactor: move contact list above copyright

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -3,6 +3,24 @@ import { FaGithub, FaLinkedinIn, FaInstagram } from 'react-icons/fa6';
 export default function Footer() {
   const currentYear = new Date().getFullYear();
 
+  const contactList = [
+    {
+      href: 'https://www.instagram.com/zl_asica/',
+      title: 'Instagram',
+      icon: <FaInstagram size={28} />,
+    },
+    {
+      href: 'https://github.com/ZL-Asica',
+      title: 'GitHub',
+      icon: <FaGithub size={28} />,
+    },
+    {
+      href: 'https://linkedin.com/in/elara-liu',
+      title: 'LinkedIn',
+      icon: <FaLinkedinIn size={28} />,
+    },
+  ];
+
   return (
     <footer
       className='mt-10'
@@ -12,40 +30,25 @@ export default function Footer() {
       }}
     >
       <div className='mx-auto max-w-7xl px-4 py-4 text-center'>
+        <div className='mb-5 flex justify-center space-x-4'>
+          {contactList.map((contact) => (
+            <a
+              key={contact.title}
+              href={contact.href}
+              title={contact.title}
+              aria-label={contact.title}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              {contact.icon}
+            </a>
+          ))}
+        </div>
         <p>
           © 2024-{currentYear} Suzu Blog.
           <br />
           All rights reserved.
         </p>
-        <div className='mt-2 flex justify-center space-x-4'>
-          <a
-            href='https://www.instagram.com/zl_asica/'
-            className='hover:text-sakuraPink'
-            aria-label='Instagram'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaInstagram size={24} />
-          </a>
-          <a
-            href='https://github.com/ZL-Asica'
-            className='hover:text-sakuraPink'
-            aria-label='GitHub'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaGithub size={24} />
-          </a>
-          <a
-            href='https://linkedin.com/in/elara-liu'
-            className='hover:text-sakuraPink'
-            aria-label='LinkedIn'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaLinkedinIn size={24} />
-          </a>
-        </div>
       </div>
     </footer>
   );
