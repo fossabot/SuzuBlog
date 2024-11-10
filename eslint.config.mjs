b46a55fb81e6b4010112e9d17a5762d75e6cfd43
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2024-11-09 18:48:10 -0600

    build: Migrate to nextjs 15

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import { zlAsicaTsReactConfig } from 'eslint-config-zl-asica';
+import nextPlugin from '@next/eslint-plugin-next';
+
+export default [
+  ...zlAsicaTsReactConfig,
+
+  // Next.js Plugin Config
+  {
+    plugins: {
+      '@next/next': nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      'unicorn/prevent-abbreviations': [
+        'error',
+        {
+          allowList: {
+            generateStaticParams: true,
+            props: true,
+            params: true,
+          },
+        },
+      ],
+    },
+  },
+];
---
+++ prettier.config.cjs
@@ -0,0 +1,4 @@
+module.exports = {
+  ...require('@zl-asica/prettier-config'),
+  plugins: ['prettier-plugin-tailwindcss'],
+};
