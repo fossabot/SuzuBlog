825c626019825e631d823e8596102e31ef5dcc5c
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2024-12-08 11:34:02 -0600

    fix: minor fix of copyright and bsky link structure.

--- src/components/article/CopyrightInfo.tsx
+++ src/components/article/CopyrightInfo.tsx
@@ -51,6 +51,7 @@ const CopyrightInfo = ({
         </p>
         <p className='mt-2'>
           {copyright.copyright.start}
+          {` `}
           <Link
             href={creativeCommons.link}
             target='_blank'
@@ -59,6 +60,7 @@ const CopyrightInfo = ({
           >
             {creativeCommons.type}
           </Link>
+          {` `}
           {copyright.copyright.end}
         </p>
       </div>
